8d64e7fe53198d068073fedc46799220bf172189
leorenis <leorenis@gmail.com>
2020-04-13 21:57:21 -0300

    [ThirdClassRoom] Correlation ids Reader report.

--- service-reader-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
+++ service-reader-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
@@ -12,7 +12,7 @@ public class ReadingReportService {
     private static final Path SOURCE = new File("service-reader-report/src/main/resources/report.txt").toPath();
     public static void main(String[] args) {
         var reportService = new ReadingReportService();
-        try (var service = new KafkaService<>(ReadingReportService.class.getSimpleName(), "USER_GENERATE_READING_REPORT", reportService::parse, User.class, Map.of())) {
+        try (var service = new KafkaService<>(ReadingReportService.class.getSimpleName(), "ECOMMERCE_USER_GENERATE_READING_REPORT", reportService::parse, User.class, Map.of())) {
             service.run();
         }
     }
