8cd3a396b6b778720162e0b580d00607241dbe0e
leorenis <leorenis@gmail.com>
2020-04-13 21:56:39 -0300

    [ThirdClassRoom] Correlation ids common kafka.

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -27,8 +27,8 @@ class KafkaDispatcher<T> implements Closeable {
         return properties;
     }
 
-    void send(String topic, String key, T payload) throws ExecutionException, InterruptedException {
-        var value = new Message<>(new CorrelationId(), payload);
+    void send(String topic, String key, CorrelationId id, T payload) throws ExecutionException, InterruptedException {
+        var value = new Message<>(id, payload);
         var record = new ProducerRecord<>(topic, key, value);
         producer.send(record, getCallback()).get();
     }
