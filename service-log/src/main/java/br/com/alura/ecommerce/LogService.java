9cd4d5b568045a5d758bc270c88c2d806a9173fe
leorenis <leorenis@gmail.com>
2020-04-15 21:47:57 -0300

    [FourthClassRoom] Architecture Fails.

--- service-log/src/main/java/br/com/alura/ecommerce/LogService.java
+++ service-log/src/main/java/br/com/alura/ecommerce/LogService.java
@@ -11,7 +11,7 @@ public class LogService {
     public static void main(String[] args) {
         var logService = new LogService();
         try(var service = new KafkaService<>(LogService.class.getSimpleName(), Pattern.compile("ECOMMERCE.*"), logService::parse,
-                String.class, Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
+                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
             service.run();
         }
     }
