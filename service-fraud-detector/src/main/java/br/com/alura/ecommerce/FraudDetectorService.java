204037dd62f411a6200ceba7add75324fb4eed17
leorenis <leorenis@gmail.com>
2020-04-15 21:47:46 -0300

    [FourthClassRoom] Architecture Fails.

--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -11,7 +11,7 @@ public class FraudDetectorService {
 
     public static void main(String[] args) {
         var fraudeService = new FraudDetectorService();
-        try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudeService::parse, Order.class, Map.of())) {
+        try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudeService::parse, Map.of())) {
             service.run();
         }
     }
