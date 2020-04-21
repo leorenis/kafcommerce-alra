b0191f873f54603c436d21e3a66a8f4d1aeeab3a
leorenis <leorenis@gmail.com>
2020-04-21 19:59:38 -0300

    [FifthClassRoom] Async retries and dead letters.

--- service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
+++ service-email/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -2,9 +2,10 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class EmailService {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         var emailService = new EmailService();
         try (var service = new KafkaService<>(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL", emailService::parse, Map.of())) {
             service.run();
