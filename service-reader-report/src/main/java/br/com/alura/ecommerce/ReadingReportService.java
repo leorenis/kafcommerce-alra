317ce0794ceabcf30d8a76f2cf46cd43c3a6cd2d
leorenis <leorenis@gmail.com>
2020-04-21 19:59:05 -0300

    [FifthClassRoom] Async retries and dead letters.

--- service-reader-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
+++ service-reader-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
@@ -5,11 +5,12 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class ReadingReportService {
 
     private static final Path SOURCE = new File("service-reader-report/src/main/resources/report.txt").toPath();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         var reportService = new ReadingReportService();
         try (var service = new KafkaService<>(ReadingReportService.class.getSimpleName(), "ECOMMERCE_USER_GENERATE_READING_REPORT", reportService::parse, Map.of())) {
             service.run();
