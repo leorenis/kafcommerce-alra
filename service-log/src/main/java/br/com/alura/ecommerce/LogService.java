6a5b356814bd43923d2e50f44ba0f452ccc75105
leorenis <leorenis@gmail.com>
2020-04-21 19:59:19 -0300

    [FifthClassRoom] Async retries and dead letters.

--- service-log/src/main/java/br/com/alura/ecommerce/LogService.java
+++ service-log/src/main/java/br/com/alura/ecommerce/LogService.java
@@ -5,10 +5,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
 
 public class LogService {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         var logService = new LogService();
         try(var service = new KafkaService<>(LogService.class.getSimpleName(), Pattern.compile("ECOMMERCE.*"), logService::parse,
                 Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
