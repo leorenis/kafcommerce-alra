eaa476dde73d580ed0d943dad8e43208a7030f21
leorenis <leorenis@gmail.com>
2020-04-22 20:24:47 -0300

    [FifthClassRoom] Async retries and dead letters.

--- common-kafka/src/main/java/br/com/alura/ecommerce/GsonDeserializer.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/GsonDeserializer.java
@@ -1,6 +1,7 @@
 package br.com.alura.ecommerce;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.kafka.common.serialization.Deserializer;
 
 public class GsonDeserializer<T> implements Deserializer<Message> {
