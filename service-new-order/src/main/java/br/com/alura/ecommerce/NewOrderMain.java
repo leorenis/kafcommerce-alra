c2caed1207585959186d4d5975e653132bb1062d
leorenis <leorenis@gmail.com>
2020-04-29 22:26:20 -0300

    [FirstClassRoom] Tidying messages and services.

--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,8 +1,9 @@
 package br.com.alura.ecommerce;
 
+import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
+
 import java.math.BigDecimal;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.IntStream;
 
 public class NewOrderMain {
@@ -16,12 +17,8 @@ public class NewOrderMain {
                     var emailMessage = "Thank you for you order! We are processing it.";
                     var emailAdress = orderId+"@gmail.com";
                     var correlationId = new CorrelationId(NewOrderMain.class.getSimpleName());
-                    try {
-                        orderDispatcher.send("ECOMMERCE_NEW_ORDER", emailAdress,correlationId, new Order(orderId, amount, emailAdress));
-                        emailDispatcher.send("ECOMMERCE_SEND_EMAIL", emailAdress,correlationId, new Email("New email", emailMessage));
-                    } catch (ExecutionException | InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    orderDispatcher.sendAsync("ECOMMERCE_NEW_ORDER", emailAdress,correlationId, new Order(orderId, amount, emailAdress));
+                    emailDispatcher.sendAsync("ECOMMERCE_SEND_EMAIL", emailAdress,correlationId, new Email("New email", emailMessage));
                 });
             }
         }
