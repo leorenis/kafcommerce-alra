a99b989b8de9915e7d3c4006633f5fe4f7c37c6d
leorenis <leorenis@gmail.com>
2020-05-09 22:59:49 -0300

    [FirstClassRoom] Tidying messages and services.

--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -8,18 +8,13 @@ import java.util.stream.IntStream;
 public class NewOrderMain {
     public static void main(String[] args) {
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
-
-                IntStream.range(0, 10).forEach( item -> {
-                    var orderId = UUID.randomUUID().toString();
-                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
-                    var emailMessage = "Thank you for you order! We are processing it.";
-                    var emailAdress = orderId+"@gmail.com";
-                    var correlationId = new CorrelationId(NewOrderMain.class.getSimpleName());
-                    orderDispatcher.sendAsync("ECOMMERCE_NEW_ORDER", emailAdress,correlationId, new Order(orderId, amount, emailAdress));
-                    emailDispatcher.sendAsync("ECOMMERCE_SEND_EMAIL", emailAdress,correlationId, new Email("New email", emailMessage));
-                });
-            }
+            IntStream.range(0, 10).forEach( item -> {
+                var orderId = UUID.randomUUID().toString();
+                var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                var emailAdress = orderId+"@gmail.com";
+                var correlationId = new CorrelationId(NewOrderMain.class.getSimpleName());
+                orderDispatcher.sendAsync("ECOMMERCE_NEW_ORDER", emailAdress,correlationId, new Order(orderId, amount, emailAdress));
+            });
         }
     }
 }
